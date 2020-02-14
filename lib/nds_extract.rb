5e0dd6641d7ddc3ebdad14c79e6702158a78d0fd
Joseph E Auz <joseph.auz77@gmail.com>
2020-02-14 02:54:41 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -67,6 +67,7 @@ def movies_with_directors_set(source)
   result = []
   while i < source.length do 
     name = source[i][:name]
+    result[i] = source[i][:movies]
     n = 0
     while n < source[i][:movies].length do
       movies = source[i][:movies][n][:title]
